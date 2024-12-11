2e4506ec0cdee65f44c0cf68923a447259872ca3
german <german.ampuero@unmsm.edu.pe>
2024-12-10 23:21:58 -0500

    add cdn quality url for images and zoom on click :D

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import FadeLoader from "react-spinners/FadeLoader";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export default function Photo({ src }: { src: string }) {
-  let [loading, setLoading] = useState(true);
+  const [imageQuality, setImageQuality] = useState(80);
+  let [imageIsLoading, setImageIsLoading] = useState(true);
+  useEffect(() => {
+    if (isMobile) {
+      console.log("Mobile");
+      setImageQuality(50);
+    }
+  }, []);
+
   const imageStyle: React.CSSProperties = {
     width: "100%",
     height: "auto",
@@ -10,19 +21,21 @@ export default function Photo({ src }: { src: string }) {
   };
   return (
     <div className="flex  justify-center">
-      {loading && <FadeLoader color="#36d7b7" className="m-3" />}
-      <img
-        src={src}
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={imageStyle}
-        loading="lazy"
-        className="mb-6 rounded"
-        onLoad={() => {
-          setLoading(false);
-        }}
-      />
+      {imageIsLoading && <FadeLoader color="#36d7b7" className="m-3" />}
+      <Zoom>
+        <img
+          src={`https://gadsw.dev/cdn-cgi/image/quality=${imageQuality}/${src}`}
+          width={imageIsLoading ? 120 : 0}
+          height={imageIsLoading ? 120 : 0}
+          sizes="100vw"
+          style={imageStyle}
+          loading="lazy"
+          className="mb-6 rounded"
+          onLoad={() => {
+            setImageIsLoading(false);
+          }}
+        />
+      </Zoom>
     </div>
   );
 }
--- components/ToggleSwitch.tsx
+++ components/ToggleSwitch.tsx
@@ -5,12 +5,13 @@ import Switch from "@mui/material/Switch";
 
 export default function ToggleSwitch() {
   const { theme, setTheme } = useTheme();
+  const changeTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <div className="flex items-center">
-      <Switch
-        defaultChecked
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      />
+      <Switch defaultChecked onClick={() => changeTheme()} />
       {theme === "dark" ? <FaSun /> : <FaRegMoon />}
     </div>
   );
